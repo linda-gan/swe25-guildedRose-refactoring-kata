72c98c02d8ebc6ab849f314bd495257b775a8817
linda-gan <linda.gan@outlook.de>
2025-10-05 22:02:09 +0200

    Reduce ifs

--- Java/src/main/java/com/gildedrose/GildedRose.java
+++ Java/src/main/java/com/gildedrose/GildedRose.java
@@ -10,46 +10,39 @@ class GildedRose {
     public void updateQuality() {
         for (Item item : items) {
             if (!item.name.equals("Aged Brie")
-                && !item.name.equals("Backstage passes to a TAFKAL80ETC concert")) {
-                if (item.quality > 0) {
-                    if (!item.name.equals("Sulfuras, Hand of Ragnaros")) {
-                        item.quality = item.quality - 1;
-                    }
+                && !item.name.equals("Backstage passes to a TAFKAL80ETC concert")
+                && !item.name.equals("Sulfuras, Hand of Ragnaros")) {
+                if (item.quality > 0) { // Decrease quality
+                    item.quality = item.quality - 1;
                 }
-            } else {
+            } else { // Increase quality
                 if (item.quality < 50) {
                     item.quality = item.quality + 1;
 
                     if (item.name.equals("Backstage passes to a TAFKAL80ETC concert")) {
-                        if (item.sellIn < 11) {
-                            if (item.quality < 50) {
-                                item.quality = item.quality + 1;
-                            }
+                        if (item.sellIn < 11 && item.quality < 50) {
+                            item.quality = item.quality + 1;
                         }
 
-                        if (item.sellIn < 6) {
-                            if (item.quality < 50) {
-                                item.quality = item.quality + 1;
-                            }
+                        if (item.sellIn < 6 && item.quality < 50) {
+                            item.quality = item.quality + 1;
                         }
                     }
                 }
             }
 
             if (!item.name.equals("Sulfuras, Hand of Ragnaros")) {
-                item.sellIn = item.sellIn - 1;
+                item.sellIn = item.sellIn - 1; // Decrease day
             }
 
-            if (item.sellIn < 0) {
+            if (item.sellIn < 0) { // Sell by date has passed
                 if (!item.name.equals("Aged Brie")) {
                     if (!item.name.equals("Backstage passes to a TAFKAL80ETC concert")) {
-                        if (item.quality > 0) {
-                            if (!item.name.equals("Sulfuras, Hand of Ragnaros")) {
-                                item.quality = item.quality - 1;
-                            }
+                        if (item.quality > 0 && !item.name.equals("Sulfuras, Hand of Ragnaros")) {
+                            item.quality = item.quality - 1;
                         }
                     } else {
-                        item.quality = item.quality - item.quality;
+                        item.quality = 0;
                     }
                 } else {
                     if (item.quality < 50) {
